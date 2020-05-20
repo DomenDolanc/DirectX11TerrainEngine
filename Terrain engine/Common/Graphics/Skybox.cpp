eb0e59c4b72913b53d7b2aa77daefeed4fa186cd
Domen Dolanc <domen.dolanc@dewesoft.com>
2020-05-20 20:37:51 +0200

    Increase max view distance

--- Terrain engine/Common/Graphics/Skybox.cpp
+++ Terrain engine/Common/Graphics/Skybox.cpp
@@ -43,7 +43,7 @@ void Skybox::CreateVertices()
     m_vertices.clear();
     m_vertexBuffer.Reset();
 
-    float halfScaling = m_scaling / 4.5f;
+    float halfScaling = m_scaling / 7.5f;
 
     VertexPositionColor vertex;
     XMFLOAT3 normal = { 0.0f, 1.0f, 0.0f };
